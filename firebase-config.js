bc8065b968f05afdd2bd084cd49dc52706780947
f00931must-hash <f00931.must@gmail.com>
2026-08-04 16:10:21 +0800

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,9 @@
 // 請將 Firebase Console > 專案設定 > 你的應用程式(Web) 的設定貼到這裡。
 export const firebaseConfig = {
-  apiKey: "PASTE_YOUR_API_KEY",
-  authDomain: "PASTE_YOUR_PROJECT.firebaseapp.com",
-  projectId: "PASTE_YOUR_PROJECT_ID",
-  storageBucket: "PASTE_YOUR_PROJECT.firebasestorage.app",
-  messagingSenderId: "PASTE_YOUR_SENDER_ID",
-  appId: "PASTE_YOUR_APP_ID"
+  apiKey: "AIzaSyDvH2-sryPBG-_TM6XT3tjZHJ6O0gfXLtI",
+  authDomain: "must-administrative-document.firebaseapp.com",
+  projectId: "must-administrative-document",
+  storageBucket: "must-administrative-document.firebasestorage.app",
+  messagingSenderId: "640842705380",
+  appId: "1:640842705380:web:f6a9a44390f31d82d9fff1"
 };
