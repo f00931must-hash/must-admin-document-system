87fe53155714b8c4be750496f965b8b2f44e0c7f
f00931must-hash <f00931.must@gmail.com>
2026-09-17 10:23:05 +0800

    feat: assemble semester ISP Word template in browser

---
+++ templates/semester-template-data/index.js
@@ -0,0 +1,8 @@
+import c1 from "./chunk1.js";
+import c2 from "./chunk2.js";
+import c3 from "./chunk3.js";
+import c4 from "./chunk4.js";
+import c5 from "./chunk5.js";
+import c6 from "./chunk6.js";
+
+export const SEMESTER_ISP_TEMPLATE_B64=c1+c2+c3+c4+c5+c6;
